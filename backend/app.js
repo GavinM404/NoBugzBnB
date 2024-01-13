a077b1dbd495cd251de5acbed8f0d3b84043ffa5
Gavin Musgrove <gavin.musgrove87@gmail.com>
2024-01-13 14:32:15 -0600

    New Framework

--- backend/app.js
+++ backend/app.js
@@ -7,23 +7,24 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+const routes = require('./routes');
 
 app.use(morgan('dev'));
+
 app.use(cookieParser());
 app.use(express.json());
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
-  }
-
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
+  // enable cors only in development
+  app.use(cors());
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
 
   // Set the _csrf token and create req.csrfToken method
   app.use(
@@ -35,3 +36,7 @@ if (!isProduction) {
       }
     })
   );
+
+app.use(routes);
+
+module.exports = app;
