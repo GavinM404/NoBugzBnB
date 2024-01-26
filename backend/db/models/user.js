0f32f71f64b91b8c47ad2b9b033f58c205731867
Gavin Musgrove <gavin.musgrove87@gmail.com>
2024-01-25 19:18:15 -0600

    catchup: associations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,6 +5,18 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
+      User.hasMany(
+        models.Spot,
+        {foreignKey: 'ownerId', onDelete: CASCADE, hooks: true }
+      );
+      User.hasMany(
+        models.Booking,
+        {foreignKey: 'userId', onDelete: CASCADE, hooks: true }
+      );
+      User.hasMany(
+        models.Reviews,
+        {foreignKey: 'userId', onDelete: CASCADE, hooks: true }
+      );
     }
   };
 
