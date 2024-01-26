8e337bdc2c7b3286f4918e59ed32ae9c0df1a766
Gavin Musgrove <gavin.musgrove87@gmail.com>
2024-01-25 20:05:33 -0600

    Fixed some reference typos

--- backend/db/migrations/20240126000142-create-review.js
+++ backend/db/migrations/20240126000142-create-review.js
@@ -10,10 +10,12 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       userId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: 'Users '},
       },
       spotId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: 'Spots' },
       },
       review: {
         type: Sequelize.STRING
