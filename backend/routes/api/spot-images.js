bffdb23942c7400f1012a3f6f369910d1ef95cc9
Gavin Musgrove <gavin.musgrove87@gmail.com>
2024-02-02 22:59:47 -0600

    finished

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { check } = require("express-validator");
+
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth");
+const {
+  Spot,
+  Review,
+  SpotImage,
+  User,
+  ReviewImage,
+  Booking,
+  sequelize,
+} = require("../../db/models");
+
+const { handleValidationErrors } = require("../../utils/validation");
+
+const router = express.Router();
+
+router.delete('/spot-images/:imageId', requireAuth, async (req, res) => {
+    const userId = req.user.id;
+    const imageId = req.params.imageId;
+
+    const spotImage = await SpotImage.findOne({
+        where: {
+            id: imageId,
+        },
+        include: {
+            model: Spot,
+            where: {
+                ownerId: userId,
+            },
+        },
+    });
+
+    if (!spotImage) {
+        res.status(404);
+        const responseObj = { message: "Spot Image couldn't be found" };
+        return res.json(responseObj);
+      }
+
+      await spot.destroy();
+
+      return res.json({ message: "Successfully deleted" });
+})
+
+module.exports = router;
